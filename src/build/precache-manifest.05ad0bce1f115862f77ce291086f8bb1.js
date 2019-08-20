15e53176acd36889aff7d374ebd09c7fb9f687ad
unknown <squaircle11@gmail.com>
2019-08-20 03:24:01 +0100

    First commit

---
+++ src/build/precache-manifest.05ad0bce1f115862f77ce291086f8bb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5285d49bfdee3ca69f41b9ee4c51eaf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4917eb7ae5bba9b6394a",
+    "url": "/static/css/main.c58b48e1.chunk.css"
+  },
+  {
+    "revision": "040553b38a6ce763f767",
+    "url": "/static/js/2.c6948f6a.chunk.js"
+  },
+  {
+    "revision": "4917eb7ae5bba9b6394a",
+    "url": "/static/js/main.947efaab.chunk.js"
+  },
+  {
+    "revision": "590b1f1a44e44eae64fb",
+    "url": "/static/js/runtime~main.8f66630e.js"
+  }
+]);
